0750934527a1685168c72ec71b2f2d9f66a56c95
David Awad <davidawad64@gmail.com>
2015-04-04 17:24:53 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+#Rust Webserver Template
+
+So I've been messing around with Rust for the past few days and I really like it. 
+
+This is a simple tutorial and boilerplate to get you started using Rust as a webserver, as it's REALLY fast at handling requests concurrently. 
+
+
+Try it out! 
+
+So the web framework being used here is called Iron. It's an awesome web framework for rust. 
+
+If you have never used it or don't have it installed use this script to install the latest version.
+
+
+```shell
+$ curl -s https://static.rust-lang.org/rustup.sh | sudo sh
+```
+
+Rust is currently at release 1.0.0 Beta, so you might want to run this occassionally as Rust is being updated a good bit.   
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate iron;
+extern crate router;
+
+// To build, $ cargo test
+// To use, go to http://127.0.0.1:3000/test
+
+use iron::{Iron, Request, Response, IronResult};
+use iron::status;
+use router::{Router};
+
+fn main() {
+     let mut router = Router::new();
+    // let's specify a handler for the landing page of our web page. We can respond to all GET requests this way. 
+    // We specify a route as the first argument of router.get and we spefify "handler" as the function to be executed. 
+     router.get("/", handler);
+    // We can accept a variable after the root as an argument, in this case its called "query"
+    // for example, "mywebserver.com/rust-iron", query would have the value "rust-iron"
+     router.get("/:query", handler);
+
+    
+    //specify iron to run on localhost port 3000
+     Iron::new(router).http("localhost:3000").unwrap();
+ 
+    // this is our handler function to be called when someone send a GET to one of our routes.
+     fn handler(req: &mut Request) -> IronResult<Response> {
+         let ref query = req.extensions.get::<Router>()
+             .unwrap().find("query").unwrap_or("/");
+        Ok(Response::with((status::Ok, *query)))
+    }
+}
